267b878577c97222b99500484bdea088b48447d5
mhayle <78674412+mhayle@users.noreply.github.com>
2023-06-04 00:39:23 -0700

    finished db script (?). wrote daily update script (also ?)
    not sure if either really work yet. db script is still running and might fail. havent even tested the update script yet

--- create_arxiv_db.py
+++ create_arxiv_db.py
@@ -1,71 +1,16 @@
 import requests
-# import feedparser
 import time
 import os
 import sqlite3
 import xml.etree.ElementTree as ET
-from datetime import timedelta
-# from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_result
+from datetime import timedelta, datetime
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
+from requests.exceptions import ConnectionError
 
 start_time = time.time()
-# base_url = 'http://export.arxiv.org/api/query?'
 
-# Search parameters
-# search_query = 'all'  # search in all categories
-# start = 50000         # start at the first result
-# total_results = 1000  # number of results to retrieve for each request
-
-# SQLite connection
-conn = sqlite3.connect('arxiv-analysis\\arxiv_metadata.db')
-
-c = conn.cursor()
-
-# Create table
-c.execute('''CREATE TABLE IF NOT EXISTS metadata
-             (arxiv_id TEXT, title TEXT, summary TEXT, subject TEXT, published TEXT, updated TEXT, authors TEXT)''')
-
-# @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=10),
-#        retry=retry_if_result(lambda result: len(result.entries) < total_results))
-# def fetch_data(start):
-#     query = f'search_query={search_query}&start={start}&max_results={total_results}'
-#     response = requests.get(base_url + query)
-#     feed = feedparser.parse(response.content)
-#     return feed
-
-# def scrape_arxiv_metadata():
-#     current_result = start
-#     while True:
-
-#         try:
-#             feed = fetch_data(current_result)
-#         except Exception as e:
-#             print(f"Fetch data failed with {str(e)}, stopping.")
-#             break
-
-#         for entry in feed.entries:
-#             arxiv_id = entry.id.split('/abs/')[-1]
-#             title = entry.title
-#             summary = entry.summary
-#             published = entry.published
-#             updated = entry.updated
-#             authors = ", ".join([author.name for author in entry.authors])
-#             subject = ", ".join([cat.term for cat in entry.tags])
-
-#             # Insert a row of data
-#             c.execute("INSERT INTO metadata VALUES (?,?,?,?,?,?,?)",
-#                       (arxiv_id, title, summary, subject, published, updated, authors))
-
-#         # Save changes
-#         conn.commit()
-
-#         # Move on to the next results
-#         current_result += total_results
-
-#         # Give website a break
-#         time.sleep(5)
-
-#         print("Time:", timedelta(seconds=time.time()-start_time))
-#         print("Papers in DB:", current_result)
+PATH = os.path.join(os.path.abspath('./'), 'arxiv-analysis')
+os.chdir(PATH)
 
 
 OAI_URL = "https://export.arxiv.org/oai2"
@@ -74,8 +19,23 @@ OAI_NAMESPACES = {
     'arXiv': 'http://arxiv.org/OAI/arXivRaw/'
 }
 
-chunk = 0
-total_records = 0
+
+# SQLite connection
+conn = sqlite3.connect('arxiv_metadata.db')
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS metadata
+             (arxiv_id TEXT, title TEXT, summary TEXT, subject TEXT, published TEXT, updated TEXT, authors TEXT)''')
+
+
+@retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=10), 
+       retry=retry_if_exception_type(ConnectionError))
+def make_request(harvest_url, parameters):
+    response = requests.get(harvest_url, params=parameters)
+    return response
+
 
 def get_record_chunk(resumptionToken=None, harvest_url=OAI_URL, metadataPrefix='arXivRaw'):
     parameters = {'verb': 'ListRecords'}
@@ -85,7 +45,7 @@ def get_record_chunk(resumptionToken=None, harvest_url=OAI_URL, metadataPrefix='
     else:
         parameters['metadataPrefix'] = metadataPrefix
 
-    response = requests.get(harvest_url, params=parameters)
+    response = make_request(harvest_url, parameters) 
 
     if response.status_code == 200:
         return response.text
@@ -104,67 +64,101 @@ def get_record_chunk(resumptionToken=None, harvest_url=OAI_URL, metadataPrefix='
             )
         )
 
+def date_parser(elm):
+    
+    dates = [
+        subnode.text
+        for node in elm.findall("arXiv:{}".format("version"), OAI_NAMESPACES)
+        for subnode in node.findall("arXiv:{}".format("date"), OAI_NAMESPACES)
+    ]
 
-def parse_record(root):
+    new_dates = [datetime.strptime(d, "%a, %d %b %Y %H:%M:%S %Z").strftime('%Y-%m-%d %H:%M:%S') for d in dates]
+    
+    published = new_dates[0]
+    updated = new_dates[len(new_dates)-1]
 
-    resumptionToken = root.find(
-        'OAI:ListRecords/OAI:resumptionToken',
-        OAI_NAMESPACES
-    )
-    resumptionToken = resumptionToken.text if resumptionToken is not None else ''
-
-    records = root.findall(
-        'OAI:ListRecords/OAI:record/OAI:metadata/arXiv:arXivRaw',
-        OAI_NAMESPACES
-    )
-
-    for r in records:
-        arxiv_id = r.find('arXiv:{}'.format('id'), OAI_NAMESPACES).text
-        title = r.find('arXiv:{}'.format('title'), OAI_NAMESPACES).text
-        abstract = r.find('arXiv:{}'.format('abstract'), OAI_NAMESPACES).text
-        
-        # published = r.find('arXiv:{}'.format('id'), OAI_NAMESPACES).text
-        # updated = r.find('arXiv:{}'.format('id'), OAI_NAMESPACES).text
-        
-        authors = r.find('arXiv:{}'.format('authors'), OAI_NAMESPACES).text
-        subjects = r.find('arXiv:{}'.format('categories'), OAI_NAMESPACES).text
-
-        records = (arxiv_id, title, abstract, published, updated, authors, subjects)
-        
-        return records, resumptionToken
+    return published, updated
 
 
+def parse_record(r):
+
+    arxiv_id = r.find('arXiv:{}'.format('id'), OAI_NAMESPACES).text
+    title = r.find('arXiv:{}'.format('title'), OAI_NAMESPACES).text
+    abstract = r.find('arXiv:{}'.format('abstract'), OAI_NAMESPACES).text
+    published, updated = date_parser(r)
+    authors = r.find('arXiv:{}'.format('authors'), OAI_NAMESPACES).text
+    subjects = r.find('arXiv:{}'.format('categories'), OAI_NAMESPACES).text
+
+    records = (arxiv_id, title, abstract, subjects, published, updated, authors)
+    
+    return records
+
+
+# def oai_harvest(resumptionToken=None, autoresume=True):
 def oai_harvest(resumptionToken=None):
     
+    calls = 0
+    total_records = 0
+
+    print("Starting downlaod...")
+
+    # tokenfile = 'resumptionToken.txt'
+
+    # if autoresume:
+    #     try:
+    #         resumptionToken = open(tokenfile, 'r').read()
+    #     except Exception as e:
+    #         print("No tokenfile found '{}'".format(tokenfile))
+    #         print("Starting download from scratch...")
+
+
     while True:
         root = ET.fromstring(get_record_chunk(resumptionToken))
-        records, resumptionToken = parse_record(root)
 
-        chunk = chunk + 1
+        resumptionToken = root.find(
+            'OAI:ListRecords/OAI:resumptionToken',
+            OAI_NAMESPACES
+        )
+
+        resumptionToken = resumptionToken.text if resumptionToken is not None else ''
+
+        records = root.findall(
+            'OAI:ListRecords/OAI:record/OAI:metadata/arXiv:arXivRaw',
+            OAI_NAMESPACES
+        )
+
+        for r in records:
+            pr = parse_record(r)
+            c.execute("INSERT INTO metadata VALUES (?,?,?,?,?,?,?)", pr)
+        
+        conn.commit()
+
+        calls = calls + 1
         total_records = total_records + len(records)
-
-        c.execute("INSERT INTO metadata VALUES (?,?,?,?,?,?,?)", records)
-
-        if resumptionToken:
-            # with open(tokenfile, 'w') as fout:
-            #     fout.write(resumptionToken)
-        else:
-            print('No resumption token, query finished')
-            return
-
+        
         print("Time:", timedelta(seconds=time.time()-start_time))
         print("Papers in DB:", total_records)
-        print("Chunks:", chunk)
+        print("API Calls:", calls)
+        
+        # if resumptionToken:
+        #     with open(tokenfile, 'w') as fout:
+        #         fout.write(resumptionToken)
+        # else:
+        #     print('No resumption token, query finished')
+        #     return
 
-        time.sleep(5)
-
+        if resumptionToken is None:
+            print('No resumption token. Done.')
+            break
+        
+        time.sleep(10)
 
 
 try:
     if __name__ == "__main__":
-        #scrape_arxiv_metadata()
         oai_harvest()
 finally:
     conn.close()
+    print("Connection Closed.")
 
 
--- update_arxiv_db.py
+++ update_arxiv_db.py
@@ -3,5 +3,81 @@ import feedparser
 import time
 import sqlite3
 from sqlite3 import Error
-from sqlite3 import datetime, timedelta
+from datetime import timedelta
+from datetime import datetime
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_result
 
+start_time = time.time()
+base_url = 'http://export.arxiv.org/api/query?'
+
+# Search parameters
+search_query = 'all'  # search in all categories
+start = 0             # start at the first result
+total_results = 1000  # number of results to retrieve for each request
+
+# SQLite connection
+conn = sqlite3.connect('arxiv-analysis\\arxiv_metadata.db')
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS metadata
+             (arxiv_id TEXT, title TEXT, summary TEXT, subject TEXT, published TEXT, updated TEXT, authors TEXT)''')
+
+@retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=10),
+       retry=retry_if_result(lambda result: len(result.entries) < total_results))
+def fetch_data(start):
+    query = f'search_query={search_query}&start={start}&max_results={total_results}&sortBy=submittedDate&sortOrder=descending'
+    response = requests.get(base_url + query)
+    feed = feedparser.parse(response.content)
+    return feed
+
+def update_arxiv_metadata():
+    current_result = start
+    looping = True
+
+    while looping:
+
+        try:
+            feed = fetch_data(current_result)
+        except Exception as e:
+            print(f"Fetch data failed with {str(e)}, stopping.")
+            break
+
+        for entry in feed.entries:
+            arxiv_id = entry.id.split('/abs/')[-1]
+            title = entry.title
+            summary = entry.summary
+            published = entry.published
+            updated = entry.updated
+            authors = ", ".join([author.name for author in entry.authors])
+            subject = ", ".join([cat.term for cat in entry.tags])
+
+            current_result += 1
+
+            if published: # parse the date if it isnt current date
+                looping = False
+                break
+
+            # Insert a row of data
+            c.execute("INSERT INTO metadata VALUES (?,?,?,?,?,?,?)",
+                      (arxiv_id, title, summary, subject, published, updated, authors))
+
+        # Save changes
+        conn.commit()
+
+        # # Move on to the next results
+        # current_result += total_results
+
+        # Give website a break
+        time.sleep(5)
+
+        print("Time:", timedelta(seconds=time.time()-start_time))
+        print("Papers added:", current_result)
+
+
+try:
+    if __name__ == "__main__":
+        update_arxiv_metadata()
+finally:
+    conn.close()
